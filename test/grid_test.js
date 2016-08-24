bcc60d778f02c0536e592372c8b25a77d60b0942
riizu <rgbatty@outlook.com>
2016-08-24 09:51:49 -0600

    Add generateCells to Grid

--- test/grid_test.js
+++ test/grid_test.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const Grid = require('../lib/grid');
+const Cell = require('../lib/cell');
 
 describe('Grid', function () {
   it('should have a 2d array matching its initialized size', function () {
@@ -11,6 +12,23 @@ describe('Grid', function () {
 
       assert.equal(grid.width, 20);
       assert.equal(grid.height, 20);
+      assert.equal(grid.cells instanceof Array, true);
+  });
+
+  it('should generate a collection of cells in a 2d array', function () {
+      var data_hash = {
+          height: 20,
+          width: 20,
+      };
+      var expectedDimensions = [data_hash.height, data_hash.width];
+      var grid = new Grid(data_hash);
+
+      var result = grid.generateCells();
+      var actualDimensions = [result.length, result[0].length];
+
+      assert.equal(result instanceof Array, true);
+      assert.deepEqual(actualDimensions, expectedDimensions);
+      assert.equal(result[0][0] instanceof Cell, true);
   });
 
   it.skip('should get a cell', function () {
