c968e585037c368acf3926bcace04c70f653a505
Parker P <exxcaelum@gmail.com>
2016-08-29 19:56:26 -0600

    update tests updated the tests to fit with the new code changes from the last PR

--- test/generator_test.js
+++ test/generator_test.js
@@ -1,21 +1,22 @@
 const assert = require('chai').assert;
 const Grid = require('../lib/grid');
 const Generator = require('../lib/generator');
+const Player = require('../lib/player')
+const DisplayHandler = require('../lib/display_handler')
 
 describe('Generator', function () {
 
-it('should not generate overlapping rooms', function(){
-    var dataHash = {
-    height: 20,
-    width: 28,
-    };
-    var grid = new Grid(dataHash);
-    var generator = new Generator(grid);    
-    grid.createRoom({x: 0, y: 0, width: 28, height: 20});
+  it('should not generate overlapping rooms', function(){
+    var player = new Player();
+    var displayHandler = new DisplayHandler({context: context});
+    var generator = new Generator(displayHandler, player);
+    // grid.createRoom({x: 0, y: 0, width: 28, height: 20});
 
-    assert.equal(grid.rooms.length, 1);
-    generator.generateRooms(92, 3, 7);
+    // assert.equal(grid.rooms.length, 1);
+    // generator.generateLevel(92, 3, 7);
 
-    assert.equal(grid.rooms.length, 1);
+    generator.generateLevel(28, 28, 10, 20, 20, 10)
+
+    assert.equal(generator.grid.rooms.length, 1);
   });
 });
